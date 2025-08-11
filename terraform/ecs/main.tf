576c830e3513880dd40f2eefdcad060d719a7908
Jacob Makil <78267963+jmakil@users.noreply.github.com>
2025-08-10 21:06:21 -0400

    Update main.tf

--- terraform/ecs/main.tf
+++ terraform/ecs/main.tf
@@ -72,6 +72,20 @@ resource "aws_iam_role_policy_attachment" "ssm_core" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# Task Role
+resource "aws_iam_role" "ecs_task_role" {
+  name = "gameshow-backend-task-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "ecs-tasks.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+
 ########################################
 # Networking: use default VPC + SG
 ########################################
@@ -170,7 +184,7 @@ resource "aws_ecs_task_definition" "backend" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-  # task_role_arn          = <add if your app needs AWS API access>
+  task_role_arn      = aws_iam_role.ecs_task_role.arn
 
   container_definitions = jsonencode([
     {
